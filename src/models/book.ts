fea1d3507268fb7af8df96a1a3ca9a9b4da14652
asphixia <william2450>
2024-02-21 23:22:43 +0100

    overload on IBook interface

--- src/models/book.ts
+++ src/models/book.ts
@@ -4,6 +4,13 @@ export interface IBook extends Document {
   title: string;
   author: string;
 }
+export interface IBook extends Document {
+    title: string;
+    author: string;
+    releaseDate: Date;
+    reviews: string[];
+    
+}
 
 const BookSchema: Schema = new Schema({
   title: { type: String, required: true },
